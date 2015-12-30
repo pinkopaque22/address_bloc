4fb29c4b65eaf03a850eaa0dc3c77ff6037937ae
Patricia Ehrhardt <pinkopaque22@gmail.com>
2015-12-30 04:39:15 +0000

    created a menu for the controller

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,23 +1,62 @@
 require_relative '../models/address_book'
+ 
+ class MenuController
+   attr_accessor :address_book
+ 
+   def initialize
+     @address_book = AddressBook.new
+   end
+ 
+   def main_menu
 
-class MenuController
-    attr_accessor :address_book
-    
-    def initialize
-        @address_book = AddressBook.new
-    end
-    
-    def main_menu
-        puts "Main Menu - #{@address_book.entries.count} entries"
-        puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
-        puts "6 - Enter your Selection:"
-        
-        
-    selection = gets.to_i
-    pust "You picked #{selection}"
-    end 
-end 
+     puts "Main Menu - #{@address_book.entries.count} entries"
+     puts "1 - View all entries"
+     puts "2 - Create an entry"
+     puts "3 - Search for an entry"
+     puts "4 - Import entries from a CSV"
+     puts "5 - Exit"
+     print "Enter your selection: "
+ 
+ 
+     selection = gets.to_i
+     puts "You picked #{selection}"
+     
+   case selection
+     when 1
+       system "clear"
+       view_all_entries
+       main_menu
+     when 2
+       system "clear"
+       create_entry
+       main_menu
+     when 3
+       system "clear"
+       search_entries
+       main_menu
+     when 4
+       system "clear"
+       read_csv
+       main_menu
+     when 5
+       puts "Good-bye!"
+       exit(0)
+     else
+       system "clear"
+       puts "Sorry, that is not a valid input"
+       main_menu
+   end
+   end
+ 
+   def view_all_entries
+   end
+ 
+   def create_entry
+   end
+ 
+   def search_entries
+   end
+ 
+   def read_csv
+   end
+ end
