76c7a36dab02fd6cc3b65202c1448d78bcdf7b1b
Patricia Ehrhardt <pinkopaque22@gmail.com>
2015-12-31 21:56:14 +0000

    checkpoint-20-menu:completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -49,9 +49,29 @@ require_relative '../models/address_book'
    end
  
    def view_all_entries
+    @address_book.entries.each do |entry|
+       system "clear"
+       puts entry.to_s
+       entry_submenu(entry)
+     end
+ 
+     system "clear"
+     puts "End of entries"
    end
  
    def create_entry
+    system "clear"
+     puts "New AddressBloc Entry"
+     print "Name: "
+     name = gets.chomp
+     print "Phone number: "
+     phone = gets.chomp
+     print "Email: "
+     email = gets.chomp
+     @address_book.add_entry(name, phone, email)
+ 
+     system "clear"
+     puts "New entry created"
    end
  
    def search_entries
@@ -60,3 +80,24 @@ require_relative '../models/address_book'
    def read_csv
    end
  end
+   def entry_submenu(entry)
+     puts "n - next entry"
+     puts "d - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+
+     selection = gets.chomp
+ 
+     case selection
+     when "n"
+     when "d"
+     when "e"
+     when "m"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       entries_submenu(entry)
+     end
+   end
