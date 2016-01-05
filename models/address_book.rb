8f1bf71fc190f4ecd71708c3fbde93916bd0c84a
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-05 23:28:42 +0000

    1st commit, add binary method, add binary spec

--- models/address_book.rb
+++ models/address_book.rb
@@ -25,8 +25,10 @@ require_relative 'entry'
      csv.each do |row|
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-     end
-  
+   end
+   end
+   def binary_search(name)
+     return nil
    end
  end
 
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../models/address_book'
-require 'csv'
+ require 'csv'
 
  RSpec.describe AddressBook do 
   let(:book) {AddressBook.new}
@@ -17,61 +17,61 @@ require 'csv'
        book_size = book.entries.size
        expect(book_size).to eq 8
      end
+    end
+     
+   describe "#binary_search" do
+     it "searches AddressBook for a non-existent entry" do
+       book.import_from_csv("entries.csv")
+       entry = book.binary_search("Dan")
+       expect(entry).to be_nil
+     end
+    end
      
      it "imports the 1st entry" do
        book.import_from_csv("entries.csv")
        entry_one = book.entries[0]
        check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
      end
-     
      it "imports the 2nd entry" do
        book.import_from_csv("entries.csv")
        entry_two = book.entries[1]
        check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
      end
-     
      it "imports the 3rd entry" do
        book.import_from_csv("entries.csv")
        entry_three = book.entries[2]
        check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
      end
-     
      it "imports the 4th entry" do
        book.import_from_csv("entries.csv")
        entry_four = book.entries[3]
        check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
      end
-     
      it "imports the 5th entry" do
        book.import_from_csv("entries.csv")
        entry_five = book.entries[4]
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
-     
-     it "imports from 6th entry" do
+     it "imports from 1st entry" do
        book.import_from_csv("entries_2.csv")
-       entry_six = book.entries[0]
+       entry_one = book.entries[0]
        puts "book  is #{book.inspect}"
-       check_entry(entry_six, "Dan", "555-555-6500", "dan@dan.com")
+       check_entry(entry_one, "Dan", "555-555-6500", "dan@dan.com")
      end
-     
-     it "imports from 7th entry" do
+     it "imports from 2nd entry" do
        book.import_from_csv("entries_2.csv")
-       entry_seven = book.entries[1]
-       check_entry(entry_seven, "Nia", "555-555-0514", "nia@nia.com")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Nia", "555-555-0514", "nia@nia.com")
      end
-     
-     it "imports from 8th entry" do
+     it "imports from 3rd entry" do
        book.import_from_csv("entries_2.csv")
-       entry_eight = book.entries[2]
-       check_entry(entry_eight, "Patricia", "555-555-0607", "patricia@patricia.com")
+       entry_three = book.entries[2]
+       check_entry(entry_thress, "Patricia", "555-555-0607", "patricia@patricia.com")
      end
      
-     
-     
-     
-   end
-  end
+
+ end
+
   
    
   
